2893122f383b7a03bbe085a68f9ee87a591c4aa5
Eric Lee <79837982+EricLee911110@users.noreply.github.com>
2023-05-03 01:16:25 +0800

    fix bug in localVerify.py

--- tools/localVerify.py
+++ tools/localVerify.py
@@ -3,15 +3,20 @@ import os
 from tqdm import tqdm
 
 ocr = ddddocr.DdddOcr(det=False, ocr=True, import_onnx_path="/content/NTUST_dddd_trainer/projects/ntust_mail/models/model.onnx", charsets_path="/content/NTUST_dddd_trainer/projects/ntust_mail/models/charsets.json")
+acc_list = []
 
 list_dir = [
-'/content/NTUST_dddd_trainer/datasets/ntust/P8x8_val',
+'/content/NTUST_dddd_trainer/datasets/ntust/ntust_val_10000',  
+'/content/NTUST_dddd_trainer/datasets/ntust/P2x2_val',
 '/content/NTUST_dddd_trainer/datasets/ntust/P4x4_val',
-'/content/NTUST_dddd_trainer/datasets/ntust/P2x2_val'
-'/content/NTUST_dddd_trainer/datasets/ntust/B3x3_val'
-'/content/NTUST_dddd_trainer/datasets/ntust/B5x5_val'
-'/content/NTUST_dddd_trainer/datasets/ntust/B7x7_val'
-'/content/NTUST_dddd_trainer/datasets/ntust/B9x9_val'
+'/content/NTUST_dddd_trainer/datasets/ntust/P8x8_val',
+'/content/NTUST_dddd_trainer/datasets/ntust/B3x3_val',
+'/content/NTUST_dddd_trainer/datasets/ntust/B5x5_val',
+'/content/NTUST_dddd_trainer/datasets/ntust/B7x7_val',
+'/content/NTUST_dddd_trainer/datasets/ntust/B9x9_val',
+'/content/NTUST_dddd_trainer/datasets/ntust/B15x15_val',
+'/content/NTUST_dddd_trainer/datasets/ntust/B45x45_val',
+'/content/NTUST_dddd_trainer/datasets/ntust/B99x99_val'
 ]
 
 
@@ -32,5 +37,9 @@ for directory in tqdm(list_dir):
 
     if res == answer:
       correct_count += 1
+    if total_count >= 1000:
+      break
 
-print(f'acc: {correct_count / total_count}')
+  accuracy = correct_count / total_count
+  acc_list.append(accuracy)
+  print(accuracy)
